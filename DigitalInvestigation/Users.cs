ec21ba5f598c4a25069dcd45ef2500f158d4f980
Door <doriszka.varkonyi@gmail.com>
2026-02-06 09:15:21 +0100

    Users don

---
+++ DigitalInvestigation/Users.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalInvestigation
+{
+	internal class Users
+	{
+		private string name;
+		private int id;
+		private string position;
+
+		public Users(string name, int id, string position)
+		{
+			this.name = name;
+			this.id = id;
+			this.position = position;
+		}
+
+		public string Name { get => name; set => name = value; }
+		public int Id { get => id; }
+		public string Position { get => position; set => position = value; }
+	}
+}
